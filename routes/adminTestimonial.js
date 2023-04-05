94e08c232f995766bd0ac78d09e2f1db6e93478d
Unicorn GTBIT <unicorngtbit@gmail.com>
2023-04-05 21:46:53 +0530

    Completed Sessions Part

--- routes/adminTestimonial.js
+++ routes/adminTestimonial.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const multer = require('multer')
 const path = require('path')
 const Testimonial = require('../model/Testimonial')
+const Session = require("../model/Session");
 const { v4: uuid4 } = require('uuid');
 const firebase = require("firebase/app");
 const admin = require("firebase-admin");
@@ -76,7 +77,6 @@ router.post('/makeTestimonial', upload.single("myFile"), async (req, res) => {
     res.status(200).send({ resCode: 200, message: "File, Testimonial Uploaded Successfully!!" });
 });
 
-
 // ----------------------------------------------- Get All Testimonials ------------------------------------------------------------------
 router.get("/getAllTestimonials", async (req, res) => {
     var testimonials = await Testimonial.find();
@@ -126,6 +126,109 @@ router.post("/deleteTestimonial", async (req, res) => {
     let testimonialFinding= await Testimonial.deleteOne({ testimonialId: testimonialId });
   
     res.status(200).send({ resCode: 200, message: "Testimonial Deleted Successfully!!" });
+});
+
+
+// ----------------------------------------------- Create Session -----------------------------------------------------------------
+router.post('/makeSession', upload.single("myFile"), async (req, res) => {
+    const storageRef = ref(storage, `sessions/${req.file.originalname}`);
+    const sessionId=uuid4();
+
+    //Forming Current Date
+    const date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    let currentDate = `${day}-${month}-${year}`;
+    // console.log(currentDate); // "17-6-2022"
+  
+    const snap=await uploadBytes(storageRef, req.file.buffer).then((snapshot) => {
+      console.log("file uploaded");
+      getDownloadURL(ref(storage, `sessions/${req.file.originalname}`)).then((url)=> {
+        console.log("URL: "+url);
+
+        try{
+          const userJson={
+              sessionId: sessionId,
+              sessionImgURL: url
+          };
+          const response=db.collection("sessions").doc(sessionId).set(userJson);
+          console.log(userJson);
+      } catch(error) {
+          console.log(error);
+      }
+    
+      });
+    });
+  
+    console.log(req.file);
+
+    const user = new Session({
+        sessionId: sessionId,
+        sessionName: req.body.sessionName,
+        sessionTime: req.body.sessionTime,
+        teacherName: req.body.teacherName,
+        sessionFee: req.body.sessionFee,
+        sessionRegisFee: req.body.sessionRegisFee,
+        sessionDuration: req.body.sessionDuration,
+        sessionDesc: req.body.sessionDesc,
+        sessionPublishDate: currentDate,
+    });
+      
+    var savedUser = await user.save();
+    res.status(200).send({ resCode: 200, message: "New Session Added Successfully!!" });
+});
+
+// ----------------------------------------------- Get All Sessions ------------------------------------------------------------------
+router.get("/getAllSessions", async (req, res) => {
+    var sessions = await Session.find();
+    let arr=[];
+
+    for(let j=0;j<sessions.length;j++)
+    {
+        let x={
+            ...sessions[j]
+        };
+        let k=x._doc;
+    
+        const snapshot=await db.collection("sessions").get();
+        const list=snapshot.docs.map((doc)=>doc.data());
+        
+        for(let i=0;i<list.length;i++)
+        {
+            if(list[i].sessionId == sessions[j].sessionId)
+            {
+                k.sessionImg= list[i].sessionImgURL;
+            }
+        }
+        arr.push(k);
+    }
+    
+    res.status(200).send({ resCode: 200, sessions: arr });
+});
+
+// ----------------------------------------------- Update Session ------------------------------------------------------------------
+router.patch("/editSession/:sessionId", async (req, res) => {
+    try {
+        var sessionId=req.params.sessionId;
+  
+        let sessionFinding = await Session.findOne({ sessionId: sessionId });
+  
+        const updateSession = await Session.findByIdAndUpdate(sessionFinding._id, req.body, {new: true});
+        res.status(200).send(updateSession);
+    }
+    catch(e) {
+        res.status(400).send(e);
+    }
+});
+
+// ----------------------------------------------- Delete Session ------------------------------------------------------------------
+router.post("/deleteSession", async (req, res) => {
+    let sessionId=req.body.sessionId;
+    let sessionFinding= await Session.deleteOne({ sessionId: sessionId });
+  
+    res.status(200).send({ resCode: 200, message: "Session Deleted Successfully!!" });
 });
 
 
